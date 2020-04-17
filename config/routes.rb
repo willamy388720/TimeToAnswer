bc0835ce27150eeaeb733610527b1bc8b42a0778
willamy388720 <willamygt@gmail.com>
2020-04-17 19:17:05 -0300

    add nome ao User_devise

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     get 'welcome/index'
   end
   
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
   devise_for :users
 
   root to: 'site/welcome#index'
