918fca9092e3211c5c38597e4e761346e367cf49
willamy388720 <willamygt@gmail.com>
2020-04-17 15:30:48 -0300

    Ativando as respostas

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,5 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.includes(:answers).where("description LIKE ?", "%#{params[:term]}%").page(params[:page])
+    @questions = Question._search_(params[:term], params[:page])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
   paginates_per 5
+
+  scope :_search_, ->(term, page){
+    includes(:answers).where("description LIKE ?", "%#{term}%").page(page)
+  }
 end
