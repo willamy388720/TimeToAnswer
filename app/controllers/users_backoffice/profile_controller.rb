12124f31e4dfe2fc392f8b187cfaf06746e27c18
willamy388720 <willamygt@gmail.com>
2020-04-18 15:22:53 -0300

    Add Foto ao Perfil

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -3,13 +3,17 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   before_action :set_user
 
   def edit
-    @user.build_user_profile if @user.user_profile.blank?
+    
   end
 
   def update
     if @user.update(params_user)
       bypass_sign_in(@user)
-      redirect_to users_backoffice_profile_path, notice: "Dados atualizado com sucesso!!"
+      if params_user[:user_profile_attributes][:avatar]
+        redirect_to users_backoffice_welcome_index_path, notice: "Avatar atualizado com sucesso!!" 
+      else
+        redirect_to users_backoffice_profile_path, notice: "Dados atualizado com sucesso!!"
+      end
     else
       render :edit
     end
@@ -28,7 +32,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
     end
 
     def params_user
-      params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, user_profile_attributes: [:id, :address, :gender, :birthdate])
+      params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, user_profile_attributes: [:id, :address, :gender, :birthdate, :avatar])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations]
   devise_for :users
 
+  get 'backoffice', to: "admins_backoffice/welcome#index"
+
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
