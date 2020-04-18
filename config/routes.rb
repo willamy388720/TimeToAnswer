e176e963185124325c75fb90085e0de05cb1fb10
willamy388720 <willamygt@gmail.com>
2020-04-17 23:18:56 -0300

    Perfil User Atualizado

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 
   namespace :users_backoffice do
     get 'welcome/index'
+    get 'profile', to: 'profile#edit'
+    patch 'profile', to: 'profile#update'
   end
   
   devise_for :admins, skip: [:registrations]
