554119845728dab4aee4bde00fd537c7b8a2334e
willamy388720 <willamygt@gmail.com>
2020-04-18 12:26:14 -0300

    Novos campos para User

--- app/helpers/users_backoffice/profile_helper.rb
+++ app/helpers/users_backoffice/profile_helper.rb
@@ -1,2 +1,13 @@
 module UsersBackoffice::ProfileHelper
+  def gender_select(user, current_gender)
+    user.user_profile.gender == current_gender ? 'btn-primary' : 'btn-default'
+  end
+
+  def verify_date(user)
+    unless user.user_profile.birthdate.blank?
+      I18n.l(user.user_profile.birthdate)
+    else
+      ""
+    end
+  end
 end
